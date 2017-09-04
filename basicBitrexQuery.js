46365905945ee2d36c8d566043b21e34dcbcb16e
Jason Wolfe <Jasonwolfe@gmail.com>
2017-09-04 16:01:32 -0700

    basic prototype working. It shows last 5 seconds of percentage gains

--- basicBitrexQuery.js
+++ basicBitrexQuery.js
@@ -7,15 +7,15 @@
 // : node basicBitrexQuery.js
 // this will dump the basic query into the console
 
-const keys = require("./keys");
+// const keys = require("./keys");
 var request = require("request-promise");
 var bittrex = require("./node.bittrex.api");
 const TEST_URI = "https://bittrex.com/api/v1.1/public/getmarketsummaries";
 
-bittrex.options({
-  apikey: keys.API_KEY,
-  apisecret: keys.API_SECRET
-});
+// bittrex.options({
+//   apikey: keys.API_KEY,
+//   apisecret: keys.API_SECRET
+// });
 
 bittrex.getmarketsummaries(function(data, err) {
   if (err) {
@@ -40,25 +40,25 @@ bittrex.getmarketsummaries(response => {
   // console.log(response);
 });
 
-bittrex.getcandles(
-  {
-    marketName: "USDT-BTC",
-    tickInterval: 5000
-  },
-  function(data, err) {
-    if (err) {
-      /**
-      {
-        success: false,
-        message: 'INVALID_TICK_INTERVAL',
-        result: null 
-      }
-    */
-      return console.error(err);
-    }
-    // console.log(data);
-  }
-);
+// bittrex.getcandles(
+//   {
+//     marketName: "USDT-BTC",
+//     tickInterval: 5000
+//   },
+//   function(data, err) {
+//     if (err) {
+//       /**
+//       {
+//         success: false,
+//         message: 'INVALID_TICK_INTERVAL',
+//         result: null
+//       }
+//     */
+//       return console.error(err);
+//     }
+//     // console.log(data);
+//   }
+// );
 
 var reqOptions = {
   method: "GET",
@@ -69,15 +69,79 @@ var reqOptions = {
   json: true
 };
 
+var previousResult = [];
+// var numbersToNamesHash = {};
+
+//load up an initial result from the API call
+request(reqOptions)
+  .then(function(parsedBody) {
+    previousResult = JSON.stringify(parsedBody, null, 2);
+    previousResult = parsedBody.result;
+    // console.log(previousResult[258]);
+    // console.log("Length = " + previousResult.length);
+  })
+  .catch(function(err) {
+    console.log("request failed : " + err);
+  });
+
+//this is the next thing returned by the interval caller
+var nextResult = [];
+
+//helper variables to store values for the comparisons
 var count = 0;
+var numbersToNamesHash = {};
+var differenceArray = [];
+var dif = 0;
+//Array.prototype.every()
+
 var intervalObject = setInterval(function() {
   request(reqOptions)
     .then(function(parsedBody) {
-      console.log("Got response : " + JSON.stringify(parsedBody, null, 2));
+      // console.log(previousResult);
+      // console.log("Should be previously result ^^^^^");
+      // console.log("Got response : " + JSON.stringify(parsedBody, null, 2));
+
+      // reset the storage arrays
+      numbersToNamesHash = {};
+      differenceArray = [];
+
+      nextResult = parsedBody.result;
+      if (previousResult.length === nextResult.length) {
+        nextResult.forEach((element, i, array) => {
+          dif = nextResult[i].Last / previousResult[i].Last * 100 - 100;
+          // console.log(dif);
+          if (dif > 0) {
+            numbersToNamesHash[dif] = element.MarketName;
+            differenceArray.push(dif);
+          }
+        });
+      } else {
+        console.log(
+          "WARNING: length match failure, different number of coins returned. Comparison failed."
+        );
+      }
+      differenceArray.sort((a, b) => {
+        return b - a;
+      });
+
+      differenceArray.forEach((elem, i, array) => {
+        console.log(
+          `${numbersToNamesHash[elem]} increased ${elem.toFixed(5)}%`
+        );
+      });
+      console.log("---------------");
+
+      // console.log(differenceArray);
+
+      previousResult = nextResult;
     })
     .catch(function(err) {
       console.log("request failed : " + err);
     });
-  console.log("Iterations: " + count);
+  // console.log("Iterations: " + count);
   count++;
-}, 1000);
+  if (count == 5) {
+    console.log(`exiting after ${count} iterations`);
+    clearInterval(intervalObject);
+  }
+}, 5000);
